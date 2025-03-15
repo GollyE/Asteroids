271e213172019824c3acd21e2c32b0d75f240fc6
GollyE <evan.golly@gmail.com>
2025-03-15 12:54:29 -1000

    C: player class completed

--- main.py
+++ main.py
@@ -10,14 +10,26 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     p1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        p1.draw(screen)
+        #for player in updatable:
+        #    player.update(dt)
+        updatable.update(dt)
+        #p1.draw(screen)
+        for player in drawable:
+            player.draw(screen)
+        #drawable.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60)/1000
         
